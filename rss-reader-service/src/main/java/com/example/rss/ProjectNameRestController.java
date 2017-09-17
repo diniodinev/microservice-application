4b8118b8694b4259d6f284a626edcc8dd47ee086
Dinyo Ilchev Dinev <dinio.dinev@gmail.com>
2017-09-17 22:17:43 +0300

    Add HATEOS Links

--- rss-reader-service/src/main/java/com/example/rss/ProjectNameRestController.java
+++ rss-reader-service/src/main/java/com/example/rss/ProjectNameRestController.java
@@ -1,20 +1,58 @@
 package com.example.rss;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.rss.resources.NewsResource;
+import com.example.rss.resources.NewsResourceAssemblers;
+import com.example.rss.service.RssReaderService;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.io.FeedException;
 
 @RestController
 @RefreshScope
 public class ProjectNameRestController {
 
-	@Value("${configuration.projectName}")
-	private String projectName;
+    @Value("${configuration.projectName}")
+    private String projectName;
 
-	@RequestMapping("/project-name")
-	public String projectName() {
-		return this.projectName;
-	}
+    @Value("${site.dnesbg.today}")
+    private String dnesbgToday;
+
+    @Value("${site.dnesbg.world}")
+    private String dnesbgWorld;
+
+    @RequestMapping("/project-name")
+    public String projectName() {
+        return this.projectName;
+    }
+
+    @Autowired
+    NewsResourceAssemblers newsResourceAssemblers;
+
+    @Autowired
+    RssReaderService rssReaderService;
+
+    @RequestMapping(value = "/dnesbg/today", method = RequestMethod.GET)
+    public NewsResource getDnesBgToday() throws IllegalArgumentException, FeedException, IOException {
+        return newsResourceAssemblers.toResource((SyndEntry) rssReaderService.redFeed(dnesbgToday).getEntries().get(0));
+    }
+
+    @RequestMapping(value = "/dnesbg/today/all", method = RequestMethod.GET)
+    public List<NewsResource> getAllNewsToday() throws IllegalArgumentException, FeedException, IOException {
+        return newsResourceAssemblers.toResources(rssReaderService.redFeed(dnesbgToday).getEntries());
+    }
+
+    @RequestMapping("/dnesbg/world")
+    public List<SyndEntry> getDnesBgWorld() throws IllegalArgumentException, FeedException, IOException {
+        return rssReaderService.redFeed(dnesbgToday).getEntries();
+    }
 
 }
