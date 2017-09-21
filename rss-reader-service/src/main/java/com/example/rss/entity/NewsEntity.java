63ca66e0e0fa867e7f0238a4c9a938beb6dfbf48
Dinyo Ilchev Dinev <dinio.dinev@gmail.com>
2017-09-21 13:46:22 +0300

    Add hibernate 2.1 capabilities.

--- rss-reader-service/src/main/java/com/example/rss/entity/NewsEntity.java
+++ rss-reader-service/src/main/java/com/example/rss/entity/NewsEntity.java
@@ -1,9 +1,26 @@
 package com.example.rss.entity;
 
+import javax.persistence.Entity;
+
+import  javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+
+@Entity
+@Table(name="NEWS_ENTITY")
 public class NewsEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "NEWS_ID")
+    private long id;
+    
+    @Column(name = "TITLE")
     private String title;
 
+    @Column(name = "URI")
     private String uri;
 
     public String getTitle() {
